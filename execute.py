7658c4926a16c8fbf9d626a30c1f95717474df1c
redenter <ashwinnsd@gmail.com>
2015-09-06 01:01:33 -0400

    wip

--- execute.py
+++ execute.py
@@ -18,9 +18,16 @@ def readData(fileName):
 def classifySurvival(trainFileName):
 	(X,Y) = prepareData(trainFileName)
 	Beta = np.random.rand(2,1)
-	prob = np.vstack((1/(1 + np.exp(-(np.dot(X,Beta))))))
-	gradient = np.dot(X.transpose(),(Y-prob))
-	print(gradient)
+	prob = lambda Beta:np.vstack((1/(1 + np.exp(-(np.dot(X,Beta))))))
+	gradient = lambda Beta:np.dot(X.transpose(),(Y-prob(Beta)))
+	W = lambda Beta:prob(Beta).dot((1-prob(Beta)).transpose())
+
+	hessian = lambda Beta: -1*(X.transpose().dot(W(Beta))).dot(X)
+
+	for i in range(1,20):
+		Beta = Beta - hessian(Beta).dot(gradient(Beta))
+
+	return Beta	
 
 def prepareData(trainFileName):	
 	(X,Y) = readData(trainFileName)
@@ -35,7 +42,12 @@ def prepareData(trainFileName):
 
 
 def predictOnTestData(testFileName, model):
-	(X,Y) = readData(testFileName, model)
+	(X,Y) = prepareData(testFileName)
+	prob = np.vstack((1/(1 + np.exp(-(np.dot(X,model))))))
+	result = [1 if x>0.5 else 0 for x in prob]
+	print(result)
 
 
-classifySurvival('train.csv')
+
+model = classifySurvival('train.csv')
+predictOnTestData('test.csv',model)
