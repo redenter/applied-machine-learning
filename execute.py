c53430ca342d719e63f449460cbda1857cfaf47e
redenter <ashwinnsd@gmail.com>
2015-09-07 13:09:43 -0400

    Gender based classification without scikit learn - using newton raphson

--- execute.py
+++ execute.py
@@ -17,22 +17,34 @@ def readData(fileName):
 
 def classifySurvival(trainFileName):
 	(X,Y) = prepareData(trainFileName)
-	Beta = np.random.rand(2,1)
-	prob = lambda Beta:np.vstack((1/(1 + np.exp(-(np.dot(X,Beta))))))
-	gradient = lambda Beta:np.dot(X.transpose(),(Y-prob(Beta)))
+	Beta = (1)*np.random.rand(2,1)
+	prob = lambda Beta:1/(1 + np.exp(-(X.dot(Beta))))
+	print(prob(Beta))
+	gradient = lambda Beta: X.transpose().dot(Y-prob(Beta))
 	W = lambda Beta:prob(Beta).dot((1-prob(Beta)).transpose())
-
+	likelihood = lambda Beta: Y.transpose().dot(np.log(prob(Beta))) + (1-Y).transpose().dot(np.log(1-prob(Beta)))
 	hessian = lambda Beta: -1*(X.transpose().dot(W(Beta))).dot(X)
+	print(np.linalg.inv(hessian(Beta)))
+	print(gradient(Beta))
+	for i in range(1,4):
+		try:
+			diff = (np.linalg.inv(hessian(Beta))).dot(gradient(Beta))
+		except:
+			break;	
+		Beta = Beta - diff
+	print(Beta)
 
-	for i in range(1,20):
-		Beta = Beta - hessian(Beta).dot(gradient(Beta))
-
+	#diff = (np.linalg.inv(hessian(Beta))).dot(gradient(Beta))
+	#	diff = 0.1*gradient(Beta)
+	#Beta = Beta - diff
+	#print(Beta)
+	print(prob(Beta))
 	return Beta	
 
 def prepareData(trainFileName):	
 	(X,Y) = readData(trainFileName)
 	size = len(X)
-	ModX = (np.array([1 if x=='male' else 0 for x in X[:,2]])).reshape(size,1)
+	ModX = (np.array([1.0 if x=='male' else 0.0 for x in X[:,2]])).reshape(size,1)
 	ones = np.vstack(np.ones(size))
 	ModX = np.concatenate((ones,ModX),1)
 	return (ModX,Y.reshape(size,1))
@@ -42,11 +54,24 @@ def prepareData(trainFileName):
 
 
 def predictOnTestData(testFileName, model):
-	(X,Y) = prepareData(testFileName)
-	prob = np.vstack((1/(1 + np.exp(-(np.dot(X,model))))))
-	result = [1 if x>0.5 else 0 for x in prob]
-	print(result)
 
+	with open(testFileName,'rb') as csvTrainFile:
+		fileReader = csv.reader(csvTrainFile)
+		next(fileReader)
+		X = []
+		for row in fileReader:
+			X.append(row[1:])
+		X = np.array(X)
+		print(X)
+	size = len(X)
+	ModX = (np.array([1.0 if x=='male' else 0.0 for x in X[:,2]])).reshape(size,1)
+	ones = np.vstack(np.ones(size))
+	ModX = np.concatenate((ones,ModX),1)
+	X = ModX
+
+	prob = lambda Beta:1/(1 + np.exp(-(X.dot(Beta))))
+
+	print(prob(model))
 
 
 model = classifySurvival('train.csv')
